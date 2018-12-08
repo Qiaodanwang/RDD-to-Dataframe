2b627f8884e50b9e941ee43dd042c947aab350d7
Qiaodan Wang <qwang32@ncsu.edu>
2018-12-07 22:21:05 -0500

    add assignment

--- src/main/java/RddListenerImpl.java
+++ src/main/java/RddListenerImpl.java
@@ -81,10 +81,10 @@ public class RddListenerImpl extends RddBaseListener {
 
         String transform() {
             if(ctx.complexexpression() == null){
-                SimpleExprCtxTransformer simpleExprCtxTransformer = new SimpleExprCtxTransformer(ctx.simpleexpression(), exprParam);
+                SimpleExprCtxTransformer simpleExprCtxTransformer = new SimpleExprCtxTransformer(ctx.simpleexpression(), 1);
                 return simpleExprCtxTransformer.transform();
             }
-            ComplexExprCtxTransformer complexExprCtxTransformer = new ComplexExprCtxTransformer(ctx.complexexpression(), exprParam);
+            ComplexExprCtxTransformer complexExprCtxTransformer = new ComplexExprCtxTransformer(ctx.complexexpression(), 1);
             return complexExprCtxTransformer.transform();
         }
     }
@@ -92,18 +92,18 @@ public class RddListenerImpl extends RddBaseListener {
     //simpleexpression
     class SimpleExprCtxTransformer{
         private RddParser.SimpleexpressionContext ctx;
-        private String simpleExprParam;
-        SimpleExprCtxTransformer(RddParser.SimpleexpressionContext ctx, String param){
+        private int nextCol;
+        SimpleExprCtxTransformer(RddParser.SimpleexpressionContext ctx, int col){
             this.ctx = ctx;
-            this.simpleExprParam = param;
+            this.nextCol = col;
         }
 
         String transform() {
             if(ctx.pureexpression() == null){
-                TupleExprCtxTransformer tupleExprCtxTransformer = new TupleExprCtxTransformer(ctx.tupleexpression(), simpleExprParam);
+                TupleExprCtxTransformer tupleExprCtxTransformer = new TupleExprCtxTransformer(ctx.tupleexpression(), nextCol);
                 return String.format("%s", tupleExprCtxTransformer.transform());
             }
-            PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(), simpleExprParam);
+            PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(), nextCol);
             return String.format("\"%s as _1\"", pureExprCtxTransformer.transfrom());
         }
     }
@@ -111,61 +111,97 @@ public class RddListenerImpl extends RddBaseListener {
     //tupleexpression
     class TupleExprCtxTransformer{
         private RddParser.TupleexpressionContext ctx;
-        private String tupleExprParam;
-        private int asId = 1;
-        TupleExprCtxTransformer(RddParser.TupleexpressionContext ctx, String param){
+        private int nextCol;
+        TupleExprCtxTransformer(RddParser.TupleexpressionContext ctx, int col){
             this.ctx = ctx;
-            this.tupleExprParam = param;
+            this.nextCol = col;
         }
 
         String transform() {
             if(ctx.tupleexpression() == null){
-                PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(0),tupleExprParam);
-                PureExprCtxTransformer pureExprCtxTransformer2 = new PureExprCtxTransformer(ctx.pureexpression(1),tupleExprParam);
-                return String.format("\"%s as _%d\", \"%s as _%d\"", pureExprCtxTransformer1.transfrom(), asId++, pureExprCtxTransformer2.transfrom(), asId++);
+                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(1),1);
+                return String.format("\"%s as _%d\"", pureExprCtxTransformer.transfrom(), nextCol++);
             }
 
-            TupleExprCtxTransformer tupleExprCtxTransformer = new TupleExprCtxTransformer(ctx.tupleexpression(), tupleExprParam);
-            PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), tupleExprParam);
+            TupleExprCtxTransformer tupleExprCtxTransformer = new TupleExprCtxTransformer(ctx.tupleexpression(), 1);
+            PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), 1);
 
             //return String.format("%s, %s as _%d", tupleExprCtxTransformer.transform(), pureExprCtxTransformer.transfrom(), asId++);
-            return String.format("%s, %s as _%d", tupleExprCtxTransformer.transform(), pureExprCtxTransformer.transfrom(), 100);
+            return String.format("%s, %s as _%d", tupleExprCtxTransformer.transform(), pureExprCtxTransformer.transfrom(), 1);
         }
     }
 
     //complexexpression
     class ComplexExprCtxTransformer{
         private RddParser.ComplexexpressionContext ctx;
-        private String complexExprParam;
-        ComplexExprCtxTransformer(RddParser.ComplexexpressionContext ctx, String param){
+        private int nextCol;
+        ComplexExprCtxTransformer(RddParser.ComplexexpressionContext ctx, int col){
             this.ctx = ctx;
-            this.complexExprParam = param;
+            this.nextCol = col;
         }
 
         String transform() {
-            if (ctx.simpleexpression()!=null){
-                SimpleExprCtxTransformer simpleExprCtxTransformer = new SimpleExprCtxTransformer(ctx.simpleexpression(), complexExprParam);
+            if (ctx.SEMICOLON()==null){
+                SimpleExprCtxTransformer simpleExprCtxTransformer = new SimpleExprCtxTransformer(ctx.simpleexpression(), nextCol);
                 return simpleExprCtxTransformer.transform();
             }
-            return "";
+            //assignments ; simple
+            AssignExprsCtxTransformer assignExprsCtxTransformer = new AssignExprsCtxTransformer(ctx.assignmentexpressions(),nextCol++);
+            SimpleExprCtxTransformer simpleExprCtxTransformer = new SimpleExprCtxTransformer(ctx.simpleexpression(),nextCol++);
+            return assignExprsCtxTransformer.transform()+","+simpleExprCtxTransformer.transform();
 
+        }
+    }
+
+    //assignmentexpressions
+    class AssignExprsCtxTransformer{
+        private RddParser.AssignmentexpressionsContext ctx;
+        private int nextCol;
+        AssignExprsCtxTransformer(RddParser.AssignmentexpressionsContext ctx, int col){
+            this.ctx = ctx;
+            this.nextCol = col;
+        }
+        String transform() {
+            //assignmentexpressions->assignmentexpression
+            if(ctx.SEMICOLON()==null){
+                AssignExprCtxTransformer assignExprCtxTransformer = new AssignExprCtxTransformer(ctx.assignmentexpression(), nextCol);
+                return assignExprCtxTransformer.transform();
+            }
+            return "";
+        }
+    }
+
+    class AssignExprCtxTransformer{
+        private RddParser.AssignmentexpressionContext ctx;
+        private int nextCol;
+        AssignExprCtxTransformer(RddParser.AssignmentexpressionContext ctx, int col){
+            this.ctx = ctx;
+            this.nextCol = col;
+        }
+        String transform() {
+            idMap.put(ctx.ID().getText(),ctx.pureexpression().getText().substring(1));
+            //test
+            System.out.println(ctx.ID().getText());
+            //test
+            System.out.println(ctx.pureexpression().getText().substring(1));
+            return "\"_1 as _"+nextCol+"\"";
         }
     }
 
     //pureexpression
     class PureExprCtxTransformer{
         private RddParser.PureexpressionContext ctx;
-        private String pureExprParam;
-        PureExprCtxTransformer(RddParser.PureexpressionContext ctx, String param){
+        private int nextCol;
+        PureExprCtxTransformer(RddParser.PureexpressionContext ctx, int col){
             this.ctx = ctx;
-            this.pureExprParam = param;
+            this.nextCol = col;
         }
 
         String transfrom() {
             if(ctx.IF()!=null) {
-                CompExprCtxTranformer compExprCtxTranformer = new CompExprCtxTranformer(ctx.comparisonexpression(), pureExprParam);
-                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), pureExprParam);
-                PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(1), pureExprParam);
+                CompExprCtxTranformer compExprCtxTranformer = new CompExprCtxTranformer(ctx.comparisonexpression(), nextCol);
+                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), nextCol);
+                PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(1), nextCol);
                 return String.format("if(%s,%s,%s)", compExprCtxTranformer.transform(), pureExprCtxTransformer.transfrom(), pureExprCtxTransformer1.transfrom());
             }
             //NUMBER
@@ -174,27 +210,22 @@ public class RddListenerImpl extends RddBaseListener {
             }
             //ID DOT ID
             else if(ctx.ID(0)!=null && ctx.DOT()!=null){
-                if(pureExprParam.equals("1")){
-                    return "_1";
-                }
-                return "_2";
+                return "_"+nextCol;
             }
             //ID
             else if(ctx.ID()!=null && ctx.OP() == null){
-                if(pureExprParam.equals("1")){
-                    return "_1";
-                }
-                return "_2";
+                String expr = idMap.getOrDefault(ctx.ID(0).getText(),"");
+                return "_"+nextCol+expr;
             }
             //(pureexpression)
             else if(ctx.LP()!=null && ctx.pureexpression(0)!=null && ctx.pureexpression(1)==null){
-                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), pureExprParam);
+                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), nextCol);
                 return String.format("(%s)", pureExprCtxTransformer.transfrom());
             }
             //pureexpression OP pureexpression
             else if(ctx.pureexpression(0)!=null && ctx.OP()!=null && ctx.pureexpression(1)!=null){
-                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), "1");
-                PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(1), "2");
+                PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), nextCol++);
+                PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(1), nextCol++);
                 return String.format("%s %s %s", pureExprCtxTransformer.transfrom(),ctx.OP().getText(),pureExprCtxTransformer1.transfrom());
             }
             return "";
@@ -206,15 +237,15 @@ public class RddListenerImpl extends RddBaseListener {
     //comparisonexpression
     class CompExprCtxTranformer{
         private RddParser.ComparisonexpressionContext ctx;
-        private String CompExprParam;
-        CompExprCtxTranformer(RddParser.ComparisonexpressionContext ctx, String param){
+        private int nextCol;
+        CompExprCtxTranformer(RddParser.ComparisonexpressionContext ctx, int col){
             this.ctx = ctx;
-            this.CompExprParam = param;
+            this.nextCol = col;
         }
 
         String transform(){
-            PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), CompExprParam);
-            PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(1), CompExprParam);
+            PureExprCtxTransformer pureExprCtxTransformer = new PureExprCtxTransformer(ctx.pureexpression(0), nextCol);
+            PureExprCtxTransformer pureExprCtxTransformer1 = new PureExprCtxTransformer(ctx.pureexpression(1), nextCol);
             return String.format("%s %s %s", pureExprCtxTransformer.transfrom(), ctx.COMP().getText(),pureExprCtxTransformer1.transfrom());
         }
     }
